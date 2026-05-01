034c74193c3c0df38f8ff19ae91e70665dfdb1cc
Norazouhtane <nozo@itu.dk>
2026-05-01 22:02:39 +0200

    formatting SHAP plot path

--- scripts/shap_analysis.py
+++ scripts/shap_analysis.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-def run_shap(model, X_test, output_dir):
+def run_shap(model, X_test, output_dir, model_name):
     """
     MAKE DOCSTRING
     """
     explainer = shap.Explainer(model)
     shap_values = explainer(X_test)
     shap.summary_plot(shap_values, X_test, max_display = 20, plot_size = 1.5, show=False)
-    beeswarm_path = os.path.join(output_dir, f"shap_beeswarm{model}.png")
+    beeswarm_path = os.path.join(output_dir, f"shap_beeswarm{model_name}.png")
     plt.savefig(beeswarm_path, dpi=150, bbox_inches="tight")
     print(f"Saved: {beeswarm_path}")
 
